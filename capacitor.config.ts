bd0e2b72a0144e54bfc2b1073190e00697099ade
Francisco Martinez <48078304+isc-francisco-martinez-Argueta@users.noreply.github.com>
2023-03-25 22:02:12 -0600

    ionic y capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tasks.project.app',
+  appName: 'project-tasks',
+  webDir: 'dist/project-tasks',
+  bundledWebRuntime: false
+};
+
+export default config;
