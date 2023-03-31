5a315d4456685a3f2f2a6fddba7fa6ddbd2c2f76
Francisco Martinez <48078304+isc-francisco-martinez-Argueta@users.noreply.github.com>
2023-03-30 23:42:12 -0600

    fixed

--- src/app/pages/person/person.component.ts
+++ src/app/pages/person/person.component.ts
@@ -11,94 +11,100 @@ import {
   templateUrl: './person.component.html',
 })
 export class PersonComponent implements OnInit {
-  persona = new FormGroup({
-    calidad: new FormControl('', [Validators.required]),
-    incidentid: new FormControl('', [Validators.required]),
-    nombre: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.pattern('[a-zA-Z]*'),
-    ]),
-    apellidoP: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.pattern('[a-zA-Z]*'),
-    ]),
-    apellidoM: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.pattern('[a-zA-Z]*'),
-    ]),
-    alias: new FormControl('', [Validators.required]),
-    nacionalidad: new FormControl('', [Validators.required]),
-    edad: new FormControl('', [
-      Validators.required,
-      Validators.pattern('[0-9]*'),
-    ]),
-    fechaNacimiento: new FormControl('', [
-      Validators.required,
-      Validators.pattern(
-        '([0-2][0-9]|3[0-1])(/|-)(0[1-9]|1[0-2])(/|-)[0-9]{4}'
-      ),
-    ]),
-    genero: new FormControl('', [Validators.required]),
-    ocupacion: new FormControl('', [Validators.required]),
-    estadoCivil: new FormControl('', [Validators.required]),
-    escolaridad: new FormControl('', [Validators.required]),
-    nombrePadre: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.pattern('[a-zA-Z]*'),
-    ]),
-    nombreMadre: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.pattern('[a-zA-Z]*'),
-    ]),
-    telefono: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
-    ]),
-    telefonoContacto: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
-    ]),
-    estatura: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^[0-9]+([.][0-9]+)?$'),
-    ]),
-    vestimenta: new FormControl('', [Validators.required]),
-    sueldoSemanal: new FormControl('', [Validators.required]),
-    direccion: new FormGroup({
-      calle: new FormControl('', [Validators.required]),
-      numero: new FormControl('', [
-        Validators.required,
-        Validators.pattern('^[0-9]+'),
-      ]),
-      colonia: new FormControl('', [
-        Validators.required,
-        Validators.pattern('[a-zA-Z]*'),
-      ]),
-      municipio: new FormControl('', [Validators.required]),
-      estado: new FormControl('', [Validators.required]),
-      postal: new FormControl('', [
-        Validators.required,
-        Validators.pattern('^[0-9]{5}$'),
-      ]),
-      lat: new FormControl('', [
-        Validators.required,
-        Validators.pattern('^-?[0-9]{1,3}(?:.[0-9]{1,10})?$'),
-      ]),
-      lng: new FormControl('', [
-        Validators.required,
-        Validators.pattern('^-?[0-9]{1,3}(?:.[0-9]{1,10})?$'),
-      ]),
-    }),
-  });
+  persona: FormGroup = new FormGroup({});
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.crearFormulario();
+  }
 
   ngOnInit(): void {}
+
+  crearFormulario() {
+    this.persona = this.fb.group({
+      calidad: new FormControl('', [Validators.required]),
+      incidentid: new FormControl('', [Validators.required]),
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('[a-zA-Z]*'),
+      ]),
+      apellidoP: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('[a-zA-Z]*'),
+      ]),
+      apellidoM: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('[a-zA-Z]*'),
+      ]),
+      alias: new FormControl('', [Validators.required]),
+      nacionalidad: new FormControl('', [Validators.required]),
+      edad: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[0-9]*'),
+      ]),
+      fechaNacimiento: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          '([0-2][0-9]|3[0-1])(/|-)(0[1-9]|1[0-2])(/|-)[0-9]{4}'
+        ),
+      ]),
+      genero: new FormControl('', [Validators.required]),
+      ocupacion: new FormControl('', [Validators.required]),
+      estadoCivil: new FormControl('', [Validators.required]),
+      escolaridad: new FormControl('', [Validators.required]),
+      nombrePadre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('[a-zA-Z]*'),
+      ]),
+      nombreMadre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('[a-zA-Z]*'),
+      ]),
+      telefono: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+      ]),
+      telefonoContacto: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+      ]),
+      estatura: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]+([.][0-9]+)?$'),
+      ]),
+      vestimenta: new FormControl('', [Validators.required]),
+      sueldoSemanal: new FormControl('', [Validators.required]),
+      direccion: new FormGroup({
+        calle: new FormControl('', [Validators.required]),
+        numero: new FormControl('', [
+          Validators.required,
+          Validators.pattern('^[0-9]+'),
+        ]),
+        colonia: new FormControl('', [
+          Validators.required,
+          Validators.pattern('[a-zA-Z]*'),
+        ]),
+        municipio: new FormControl('', [Validators.required]),
+        estado: new FormControl('', [Validators.required]),
+        postal: new FormControl('', [
+          Validators.required,
+          Validators.pattern('^[0-9]{5}$'),
+        ]),
+        lat: new FormControl('', [
+          Validators.required,
+          Validators.pattern('^-?[0-9]{1,3}(?:.[0-9]{1,10})?$'),
+        ]),
+        lng: new FormControl('', [
+          Validators.required,
+          Validators.pattern('^-?[0-9]{1,3}(?:.[0-9]{1,10})?$'),
+        ]),
+      }),
+    });
+  }
 
   get nombreNoValido() {
     return (
