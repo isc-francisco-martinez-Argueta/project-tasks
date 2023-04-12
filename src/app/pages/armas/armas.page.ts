4300e362e12cc0597493d60ed85fb7e96661465a
“Francisco <48078304+isc-francisco-martinez-Argueta@users.noreply.github.com>
2023-04-12 14:45:28 -0600

    modullo armas listo

--- src/app/pages/armas/armas.page.ts
+++ src/app/pages/armas/armas.page.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-armas',
-  templateUrl: './armas.page.html',
-  styleUrls: ['./armas.page.scss'],
+    selector: "app-armas",
+    templateUrl: "./armas.page.html",
+    styleUrls: ["./armas.page.scss"],
 })
 export class ArmasPage implements OnInit {
+    armas: FormGroup = new FormGroup({});
+    constructor(private fb: FormBuilder) {
+        this.crearFormulario();
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
+    crearFormulario() {
+        this.armas = this.fb.group({
+            nombre: ["", [Validators.required]],
+            calidad: ["", [Validators.required]],
+            cantidad: ["", [Validators.required]],
+            tipo: ["", [Validators.required]],
+            calibre: ["", [Validators.required]],
+            matricula: [
+                "",
+                [Validators.required, Validators.pattern("[a-zA-Z0-9]*")],
+            ],
+            fabricante: ["", [Validators.required]],
+            noSerie: [
+                "",
+                [Validators.required, Validators.pattern("[a-zA-Z0-9]*")],
+            ],
+            modelo: ["", [Validators.required]],
+            archivos: ["", [Validators.required]],
+            observaciones: ["", [Validators.required]],
+        });
+    }
+    onSubmit() {
+        if (this.armas.invalid) {
+            return Object.values(this.armas.controls).forEach((control) => {
+                if (control instanceof FormGroup) {
+                    Object.values(control.controls).forEach((control) =>
+                        control.markAllAsTouched()
+                    );
+                } else {
+                    control.markAsTouched();
+                }
+            });
+        }
+        console.log(this.armas.value);
+    }
 }
