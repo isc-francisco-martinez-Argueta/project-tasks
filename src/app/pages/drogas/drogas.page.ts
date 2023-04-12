d7eb0179708c226aebfd651d8ff60049e00f84fe
“Francisco <48078304+isc-francisco-martinez-Argueta@users.noreply.github.com>
2023-04-12 13:47:37 -0600

    modulo drogas listo

--- src/app/pages/drogas/drogas.page.ts
+++ src/app/pages/drogas/drogas.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-drogas',
-  templateUrl: './drogas.page.html',
-  styleUrls: ['./drogas.page.scss'],
+    selector: "app-drogas",
+    templateUrl: "./drogas.page.html",
+    styleUrls: ["./drogas.page.scss"],
 })
 export class DrogasPage implements OnInit {
+    drogas: FormGroup = new FormGroup({});
+    constructor(private fb: FormBuilder) {
+        this.crearFormulario();
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
+    crearFormulario() {
+        this.drogas = this.fb.group({
+            tipo: ["", [Validators.required]],
+            calidad: ["", [Validators.required]],
+            cantidad: ["", [Validators.required]],
+            unidad: ["", [Validators.required]],
+            observaciones: ["", [Validators.required]],
+        });
+    }
+    onSubmit() {
+        if (this.drogas.invalid) {
+            return Object.values(this.drogas.controls).forEach((control) => {
+                if (control instanceof FormGroup) {
+                    Object.values(control.controls).forEach((control) =>
+                        control.markAllAsTouched()
+                    );
+                } else {
+                    control.markAsTouched();
+                }
+            });
+        }
+        console.log(this.drogas.value);
+    }
 }
